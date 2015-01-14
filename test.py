16d331a580d314f4b99e62970a019988e150cd9d
Hao Liu <mouse.mliu@gmail.com>
2015-01-14 23:57:59 +0800

    remove useless test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import urllib.parse
-from xml.sax.saxutils import unescape
-
-name = "Custom%3A Sales Profile"
-name = urllib.parse.unquote(unescape(name, {"&apos;": "'", "&quot;": '"'}))
-print (name)
