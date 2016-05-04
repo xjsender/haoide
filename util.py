cc502c56a4cb4b19fb7b0fbbf489ed7a5463b7e8
Hao Liu <mouse.mliu@gmail.com>
2016-05-04 21:54:40 +0800

    fix bug for viewing debug log

--- util.py
+++ util.py
@@ -1960,6 +1960,7 @@ def parse_sync_test_coverage(result):
     codeCoverage = result["codeCoverage"]
 
     allrows = []
+    print (json.dumps(result))
     if result["failures"]:
         allrows.append("Failed Test Methods:")
         for failure in sorted(result["failures"], key=lambda k : k["name"]):
