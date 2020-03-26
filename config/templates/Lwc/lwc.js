d839c3fa7fc836357d5872c55084155161c9c5c6
Lushang <tyler@lushang.me>
2020-03-26 13:22:29 +0800

    fix git directory recognizing bug step 1

--- config/templates/Lwc/lwc.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, track } from 'lwc';
-
-export default class Hello extends LightningElement {
-    @track greeting = 'World';
-}
--- config/templates/lwc/lwc.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, track, wire } from 'lwc';
-
-export default class {class_name__c} extends LightningElement {
-
-}
